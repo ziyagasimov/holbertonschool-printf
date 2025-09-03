487507c9022d1d972deee865fc0130e88bd31836
ziyagasimov <ziyagasimov0@gmail.com>
2025-09-03 13:04:50 +0000

    jhfhv

---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int my_printf(const char *format, ...);
+
+#endif /* MAIN_H */
+
--- my_printf1
+++ my_printf1
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
 
